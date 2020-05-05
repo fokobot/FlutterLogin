b98d6c9118bc76e940c55ca1b02f5055f4632d79
Luis Manuel Cárcamo Morales <luismanuelcarcamo@gmail.com>
2020-05-05 15:11:36 -0500

    Información del usuario en la pantalla de inicio

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,13 +9,14 @@ import 'package:http/http.dart' as http;
 
 class UserModel extends ChangeNotifier {
   final String token;
-  final String username;
-  final String name;
+  String username;
+  String name;
   bool status = false;
 
   UserModel({this.token, this.username, this.name});
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
+  developer.log('log me', name: 'DEBUG');
     return UserModel(
       token: json['token'],
       username: json['username'],
@@ -36,23 +39,17 @@ class UserModel extends ChangeNotifier {
     if (response.statusCode == 200) {
       print('${response.body}');
       notifyListeners();
-      return UserModel.fromJson(json.decode(response.body));
+      var user = UserModel.fromJson(json.decode(response.body));
+      this.name = user.name;
+      this.username = user.username;
+      this.status = true;
+      return user;
     } else {
       print("signIn failed");
       print('${response.body}');
       throw Exception(response.body);
     }
   }
-  /*Future<void> signIn(String email, String password) async {
-    if (this.username == email) {
-      this.status = true;
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setBool('status', this.status);
-      notifyListeners();
-    } else {
-      notifyListeners();
-    }
-  }*/
 
   Future<void> verifyStatus() async {
     final prefs = await SharedPreferences.getInstance();
